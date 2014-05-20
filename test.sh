9e85bdaf695db3e249cb45424c095b4476bd7469
Christopher J Lyth <cjlyth@gmail.com>
2014-05-20 00:02:47 -0400

    added a post line to the script following the tutorial

--- test.sh
+++ test.sh
@@ -9,8 +9,13 @@ docker build -t solr/dist:4.8.1 solr-4.8.1
 docker build -t solr/dist:4.5.1 solr-4.5.1
 docker build -t cjlyth/solr solr
 
-docker run --rm -i cjlyth/solr
+docker stop local_solr 2>/dev/null | xargs --no-run-if-empty docker rm
 
-# 
+docker run -d --name local_solr cjlyth/solr 
+
+docker run -ti --rm cjlyth/solr java -jar exampledocs/post.jar -h
+# java -Dauto -jar post.jar *
+# docker run -ti --rm cjlyth/solr
+ 
 # docker run --rm -v $(pwd):/download solr/dist:4.5.1 cp /opt/solr/example/solr/collection1/conf/solrconfig.xml /download/solrconfig-4.5.1.xml
 # docker run --rm -v $(pwd):/download solr/dist:4.8.1 cp /opt/solr/example/solr/collection1/conf/solrconfig.xml /download/solrconfig-4.8.1.xml
